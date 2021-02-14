98435eb61130e50cd07285135b298060bafbee74
kesa <nekepatri@gmail.com>
2021-02-14 13:54:33 +0300

    Object Literal Assignment

--- assignments/objects.js
+++ assignments/objects.js
@@ -5,17 +5,13 @@
    A comma separates each name-value pair from the next.
    A comma after the last name-value pair is optional.
 */
-
 var myObject = {
-    fullname: "Patricia Nekesa",
-    greet: (message, name) => {
-        console.log(message + " " + name + "!!");
-
-    },
-
-   
+  fullname: "Patricia Nekesa",
+  greet: (message, name) => {
+    console.log(message + " " + name + "!!");
+  },
 };
 
- // Use/access the created object literal
- console.log(myObject.fullname);
- myObject.greet("Greetings", myObject.fullname)
+// Use/access the created object literal
+console.log(myObject.fullname);
+myObject.greet("Greetings", myObject.fullname);
